fb1a5b768cd1b31c18a49d3fb035de8c4fa7d2e0
rakipov <rakipovs@gmail.com>
2020-10-13 19:11:02 +0300

    feat: add index, landing and stats functions

--- landing/app/views.py
+++ landing/app/views.py
@@ -12,7 +12,12 @@ counter_click = Counter()
 
 def index(request):
     # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
-    return render('index.html')
+    param = request.GET.get('from-landing')
+    if param:
+        counter_click.update({param})
+        print(param)
+    print(sum(counter_click.values()))
+    return render(request, 'index.html')
 
 
 def landing(request):
@@ -20,13 +25,28 @@ def landing(request):
     # в зависимости от GET параметра ab-test-arg
     # который может принимать значения original и test
     # Так же реализуйте логику подсчета количества показов
-    return render('landing.html')
+    param = request.GET.get('ab-test-arg')
+    counter_show.update({param})
+    if param == 'test':
+        return render(request, 'landing_alternate.html')
+    elif param == 'original':
+        return render(request, 'landing.html')
+    else:
+        return render(request, 'index.html')
 
 
 def stats(request):
     # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
     # Для вывода результат передайте в следующем формате:
-    return render('stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+    try:
+        test_lable = counter_click['test'] / counter_show['test']
+    except:
+        test_lable = 0.0
+    try:
+        original_lable = counter_click['original'] / counter_show['original']
+    except:
+        original_lable = 0.0
+    return render(request, 'stats.html', context={
+        'test_conversion': test_lable,
+        'original_conversion': original_lable,
     })
